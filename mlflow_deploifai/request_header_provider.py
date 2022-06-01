d06f9bedef32d6fc5c016f72c2a7fa095403feef
Sean Chok <seanchok@gmail.com>
2022-06-01 16:25:21 +0800

    generate uuid if experiment run id is not present in environment

--- mlflow_deploifai/request_header_provider.py
+++ mlflow_deploifai/request_header_provider.py
@@ -1,4 +1,5 @@
 import os
+import uuid
 from mlflow.tracking.request_header.abstract_request_header_provider import (
     RequestHeaderProvider,
 )
@@ -6,15 +7,17 @@ from mlflow.tracking.fluent import active_run, _get_experiment_id, get_experimen
 
 
 class DeploifaiRequestHeaderProvider(RequestHeaderProvider):
+    # set class variable to identify deploifai experiment run so that all mlflow runs get associated with the same deploifai experiment run
+    # if USER_EXPERIMENT_RUN_ID environment variable is not set, generate a new deploifai experiment run using a new uuid
+    _deploifai_run_id = os.environ.get("USER_EXPERIMENT_RUN_ID", default=str(uuid.uuid4()))
+
     def in_context(self):
         return True
 
     def request_headers(self):
         added_headers = {}
 
-        _deploifai_run_id = os.environ.get("USER_EXPERIMENT_RUN_ID", default="")
-        if _deploifai_run_id is not "":
-            added_headers.update({"deploifai-runid": _deploifai_run_id})
+        added_headers.update({"deploifai-runid": self._deploifai_run_id})
 
         _run = active_run()
         if _run is not None:
