4dd1fef25e43c9f524596a607083011478d0dec4
Sean Chok <seanchok@gmail.com>
2022-06-01 16:26:32 +0800

    refactor examples and readme

--- example/main.py
+++ example/main.py
@@ -2,21 +2,21 @@ import os
 import mlflow
 from time import sleep
 
-os.environ["MLFLOW_TRACKING_USERNAME"] = "user"
-os.environ["MLFLOW_TRACKING_PASSWORD"] = "password"
+# only if running an experiment outside deploifai (this must come before setting mlflow tracking uri and experiment)
+os.environ["MLFLOW_TRACKING_USERNAME"] = "<workspace>/<project>/<experiment>"
+os.environ["MLFLOW_TRACKING_PASSWORD"] = "drat-token"
+# setup mlflow for this experiment
+mlflow.set_tracking_uri('https://community.mlflow.deploif.ai')
+mlflow.set_experiment("<workspace>/<project>/<experiment>")
 
-# mlflow.create_experiment("test")
-mlflow.set_tracking_uri("http://localhost")
-mlflow.set_experiment(experiment_id="1")
 
-
-def main():
+def main(limit):
   with mlflow.start_run():
-    for i in range(1, 100):
+    for i in range(1, limit):
       mlflow.log_metric("key", i)
       print(i)
       sleep(1)
 
-
 if __name__ == "__main__":
-  main()
+  main(10)
+  main(20)
